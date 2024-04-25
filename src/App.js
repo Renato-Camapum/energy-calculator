2fef67d2274d497ebefbbe619ad2cce10db6b3f4
Renato  Camapum <renatocamapum@warmup.com>
2024-04-25 15:19:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './costCalculator.css';
+import CostCalculator from './costCalculator/CostCalculator';
+
+function App() {
+  return (
+    <div>
+      <CostCalculator />
+    </div>
+  );
+}
+
+export default App;
