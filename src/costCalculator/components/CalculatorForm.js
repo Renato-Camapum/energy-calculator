895fa2a88bbb2145d6f8bdabae1778f34f6d5ac4
Renato  Camapum <renatocamapum@warmup.com>
2024-05-02 11:52:41 +0100

    changes applied

--- src/costCalculator/components/CalculatorForm.js
+++ src/costCalculator/components/CalculatorForm.js
@@ -1,80 +1,86 @@
-import React, { useEffect } from 'react';
-import roomData from '../data/roomData';
-import CostCalculator from '../CostCalculator';
+import React, { useEffect } from "react";
+import axios from "axios";
 
+function CalculatorForm({
+  selectedRoomType,
+  setSelectedRoomType,
+  selectedProjectSize,
+  setSelectedProjectSize,
+  email,
+  setEmail,
+  handleSubmit,
+  setRoomTypeChanged,
+  setProjectSizeChanged,
+}) {
 
-
-
-function CalculatorForm({ selectedRoomType, setSelectedRoomType, selectedProjectSize, setSelectedProjectSize, email, setEmail, handleSubmit,setRoomTypeChanged,
-    setProjectSizeChanged }) {
   return (
-    <form action='' method='' id="calculator-form" onSubmit={handleSubmit}>
-         <div className="pull-left text-large col-md-6 padding-left-0">
-                <div className="row">
-                  <div className="column question">
-                    <h3 className='question_name'> Room Type:</h3>
-                    <div className="button dropdown">
-                      <select
-                        id="room-type"
-                        value={selectedRoomType}
-                        onChange={(e) => {setSelectedRoomType(e.target.value);
-                          setRoomTypeChanged(true);
-                        }}
-                      >
-                        <option value="bathroom">Bathroom / WC</option>
-                        <option value="kitchen">Kitchen / Utility</option>
-                        <option value="bedroom">Bedroom</option>
-                        <option value="livingroom">Living Room / Dining Room</option>
-                        <option value="conservatory">Conservatory / Porch</option>
-                        <option value="hallway">Hallway / Landing</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="column question">
-                    <h3 className='question_name'> Project Size:</h3>
-                    <div className="button input_lfield">
-                      <input
-                        id="project-size"
-                        placeholder="In meters squared"
-                        required
-                        value={selectedProjectSize}
-                        onChange={(e) => {setSelectedProjectSize(e.target.value);
-                        setProjectSizeChanged(true);
-                        }}
-                      >
-                        
-                      </input>
-                    </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="column question">
-                    <h3 className='question_name'> Email Address:</h3>
-                    <div className="button input_lfield">
-                      <input
-                        type="email"
-                        id="email"
-                        placeholder="Enter your email..."
-                        required
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                      />
-                    </div>
-                  </div>
-                  <div className="column question">
-                    <input
-                      type="submit"
-                      value="Calculate Costs"
-                      id="calculate-button-submit"
-                      className="calculate-button"
-                    />
-                  </div>
-                </div>
-              </div>
+    <form action="" method="" id="calculator-form" onSubmit={handleSubmit}>
+      <div className="pull-left text-large col-md-6 padding-left-0">
+        <div className="row">
+          <div className="column question">
+            <h3 className="question_name"> Room Type:</h3>
+            <div className="button dropdown">
+              <select
+                id="room-type"
+                value={selectedRoomType}
+                onChange={(e) => {
+                  setSelectedRoomType(e.target.value);
+                  setRoomTypeChanged(true);
+                }}
+              >
+                <option value="bathroom">Bathroom / WC</option>
+                <option value="kitchen">Kitchen / Utility</option>
+                <option value="bedroom">Bedroom</option>
+                <option value="livingroom">Living Room / Dining Room</option>
+                <option value="conservatory">Conservatory / Porch</option>
+                <option value="hallway">Hallway / Landing</option>
+              </select>
+            </div>
+          </div>
+          <div className="column question">
+            <h3 className="question_name"> Project Size: ( m<sup>2</sup> )</h3>
+            <div className="button input_lfield">
+              <input
+                id="project-size"
+                placeholder="In meters squared"
+                required
+                value={selectedProjectSize}
+                onChange={(e) => {
+                  setSelectedProjectSize(e.target.value);
+                  setProjectSizeChanged(true);
+                }}
+              ></input>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="column question">
+            <h3 className="question_name"> Email Address:</h3>
+            <div className="button input_lfield">
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email..."
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="column question">
+            <input
+              type="submit"
+              value="Calculate Costs"
+              id="calculate-button-submit"
+              className="calculate-button"
+            />
+          </div>
+        </div>
+      </div>
     </form>
   );
-  
 }
+
 
 
 export default CalculatorForm;
--- src/costCalculator/data/roomData.js
+++ src/costCalculator/data/roomData.js
@@ -1,30 +1,36 @@
 const roomData = {
   bathroom: {
+    name: 'Bathroom / WC',
     elec_watt: 118.5,
     gas_watt: 119.3,
     r_watt: 139.3,
   },
   kitchen: {
+    name: 'Kitchen / Utility',
     elec_watt: 52.9,
     gas_watt: 53.2,
     r_watt: 72.7,
   },
   bedroom: {
+    name: 'Bedroom',
     elec_watt: 50.0,
     gas_watt: 51.2,
     r_watt: 75.3,
   },
   livingroom: {
+    name: 'Living Room / Dining Room',
       elec_watt: 55.7,
       gas_watt: 55.9,
       r_watt: 79.2,
     },
   conservatory: {
+    name: 'Conservatory / Porch',
       elec_watt: 0,
       gas_watt: 0,
       r_watt: 0,
     },
   hallway: {
+    name: 'Hallway / Landing',
       elec_watt: 46.8,
       gas_watt: 47.0,
       r_watt: 0,
